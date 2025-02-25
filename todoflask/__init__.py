eb8f23f6b22aa39c0f4856accc92f4cb54d86ac9
Javier Uc Ix <javier.uc@outlook.com>
2025-02-25 15:04:07 -0600

    Agregar soporte para autenticación de usuarios y mejorar las plantillas de inicio de sesión y registro

--- todoflask/__init__.py
+++ todoflask/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
 from .views import view_todo, view_auth
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
 	app = Flask(__name__)
-
+	db = SQLAlchemy()
 	#configuracion de la aplicacion
 	app.config.from_mapping(
 		DEBUG = True,
 		SECRET_KEY = 'dev',
+		SQLALCHEMY_DATABASE_URI = 'sqlite:///todolist.db'
 	)
-
+	db.init_app(app)
 	#Registrar blueprints
 
 	app.register_blueprint(view_todo.bp)
@@ -17,5 +19,9 @@ def create_app():
 	@app.route('/')
 	def index():
 		return render_template('index.html')
+
+	with app.app_context():
+		db.create_all()
+
 	return app
 
---
+++ todoflask/models.py
@@ -0,0 +1,12 @@
+from todoflask import db
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(25), unique=True, nullable=False)
+	password = db.Column(db.String(40), nullable=False)
+
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+	def __repr__(self):
+		return f"<User: {self.username} >"
